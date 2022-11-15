73cd420cf017e5518d143dec5fef4ead39484fa4
cowanweks <cowannwekesa@gmail.com>
2022-11-15 15:42:54 +0300

    Final commit

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let btnsignup = document.querySelector('button.register');	
     let dropdown = document.getElementById('dropdown');
 
+    let user = document.querySelector('#user')
+    let technician = document.querySelector('#technician')
+    let company = document.querySelector('#company')
+
+
     btnsignup.addEventListener('click',() => {
         dropdown.click();
     })
